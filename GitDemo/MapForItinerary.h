304b9978c20ea78eb9793f1e1a415cb3e9453931
Prema Janoti <prema.janoti@Jitendras-Mac-mini-2.local>
2013-09-18 12:10:13 +0530

    Added new baranch

---
+++ GitDemo/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitDemo
+//
+//  Created by Prema Janoti on 18/09/13.
+//  Copyright (c) 2013 Prema Janoti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
