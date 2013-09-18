cf06b0883e2bcebbe233575431530f3765af6981
Prema Janoti <prema.janoti@Jitendras-Mac-mini-2.local>
2013-09-18 11:53:09 +0530

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Prema Janoti on 18/09/13.
+//  Copyright (c) 2013 Prema Janoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Prema Janoti on 18/09/13.
+//  Copyright (c) 2013 Prema Janoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
